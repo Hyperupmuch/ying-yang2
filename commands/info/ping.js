ceb408b4a7c64b4a9ff7b0db0e3106f0567d3363
Hyperupmuch <71985983+Hyperupmuch@users.noreply.github.com>
2020-10-31 07:23:34 -0400

    .

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,11 +1,23 @@
+const Discord = require("discord.js");
 module.exports = {
-    name: "ping",
-    description: "Returns latency and API ping",
-    run: async (client, message, args) => {
-        const msg = await message.channel.send(`🏓 Pinging....`);
+  name: "ping",
+  category: "info",
 
-        msg.edit(`🏓 Pong!
-        Latency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms
-        API Latency is ${Math.round(client.ping)}ms`);
-    }
-}
+  description: "Returns latency and API ping",
+  timeout: 10000,
+  run: async (bot, message, args) => {
+    message.channel.send(`🏓 Pinging....`).then((msg) => {
+      const _ = new Discord.MessageEmbed()
+        .setTitle("Pong!")
+        .setDescription(
+          `🏓 Pong!\nLatency is ${Math.floor(
+            msg.createdTimestamp - message.createdTimestamp
+          )}ms\nAPI Latency is ${Math.round(bot.ws.ping)}ms`
+        )
+        .setColor("RANDOM");
+      msg.edit(_);
+      msg.edit("\u200B");
+    });
+  },
+};
+//Pong ping !!!
--- functions.js
+++ functions.js
@@ -41,3 +41,20 @@ module.exports = {
             .then(collected => collected.first() && collected.first().emoji.name);
     }
 };
+
+module.exports = {
+    calculator: function (num1, operator, num2) {
+      if (!num1) throw new TypeError("Missing num1");
+      if (!operator) throw new TypeError("Missing operator");
+      if (!num2) throw new TypeError("Missing num2");
+      if (operator == "*") return num1 * num2;
+      if (operator == "^") return num1 ^ num2;
+      if (operator == "+") return num1 + num2;
+      if (operator == "-") return num1 - num2;
+      if (operator == "/") return num1 / num2;
+    },
+    formatDate: function (date) {
+      return new Intl.DateTimeFormat("en-US").format(date);
+    },
+  };
+  
